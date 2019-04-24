8b76103c6f29ff7c04d7230840056079cbcd0a63
Ken Crawford <49908215+KenCraw4ord@users.noreply.github.com>
2019-04-24 10:23:17 -0400

    Added a Comment
    Added a new comment on local git repo, then pushed to publish

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.helloworld
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
+// THIS IS MY NEW COMMENT THAT I AM PUSHING TO GIT FROM LOCAL REPOSITORY CHANGE, THEN UPLOADED
+
 //Class in Kotlin , name is MainActivity
 class MainActivity : AppCompatActivity() {
 // on create function
